71c79b21d0e68f4c424b2ef12110907d510320e1
pavithrapn49 <pavithrapn49@gmail.com>
2026-01-07 19:49:01 +0530

    env added

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,7 @@ API.interceptors.request.use(
   (req) => {
     const token = localStorage.getItem("token");
     if (token) {
-      req.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return req;
   },
